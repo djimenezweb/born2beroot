3c7492593bb8c1084b33e3b01472f6b046c2feda
Daniel J. <117752348+djimenezweb@users.noreply.github.com>
2025-05-05 21:11:16 +0200

    Avoid expr, use ip and hostname instead of ifconfig

--- monitoring.sh
+++ monitoring.sh
@@ -7,14 +7,16 @@ cpu_phys="$(nproc)"
 cpu_sockets="$(lscpu | grep "Socket(s)" | awk '{print $2}')"
 cpu_cores="$(lscpu | grep "Core(s) per socket" | awk '{print $2}')"
 cpu_threads="$(lscpu | grep "Thread(s) per socket" | awk '{print $2}')"
-cpu_virtual="$(expr $cpu_sockets \* $cpu_cores \* $cpu_threads)"
+# Avoid expr cpu_virtual="$(expr $cpu_sockets \* $cpu_cores \* $cpu_threads)"
+cpu_virtual="$(( cpu_sockets * cpu_cores * cpu_threads ))"
 
 # MEMORY
 # 7th column = Available memory
 # 2nd column = Total memory
 mem_avail="$(free -m | grep "Mem" | awk '{print $7}' | xargs)"
 mem_total="$(free -m | grep "Mem" | awk '{print $2}' | xargs)"
-mem_pcent="$(expr 100 \* $mem_avail / $mem_total | xargs)"
+# Avoid expr mem_pcent="$(expr 100 \* $mem_avail / $mem_total | xargs)"
+mem_pcent="$(( 100 * mem_avail / mem_total ))"
 
 # DISK
 dsk_avail="$(df -BM --output='avail' . | awk 'NR==2' | xargs)"
@@ -23,6 +25,9 @@ dsk_pcent="$(df --output='pcent' . | awk 'NR==2' | xargs)"
 
 # NETWORK
 # ifconfig is not installed by default on Debian
+# Better use this:
+# ip_addr=$(hostname -I | awk '{print $1}')
+# mac_addr=$(ip link show | awk '/ether/ {print $2; exit}')
 ip_addr="$(ifconfig | grep inet | awk 'NR==1 {print $2}')"
 mac_addr="$(ifconfig | grep ether | awk 'NR==1 {print $2}')"
 
